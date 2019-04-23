2ecfebffc529828bf1e2b77a6f97598371e257bb
name <email>
2019-04-23 21:36:25 +0800

    更新地址

--- usercontroller/src/main/java/com/fh/user/controller/UserController.java
+++ usercontroller/src/main/java/com/fh/user/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     @Reference//dubbo下的Reference 表示从zookeeper上面拿到引用
